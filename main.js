9f8e894f1222da2ade246f8cfa24a110a2644981
Xpwnstar78 <xpwnstar78@gmail.com>
2020-11-02 23:38:38 -0500

    initial commit

--- main.js
+++ main.js
@@ -3,7 +3,10 @@
 // and odd numbers
 // Using the spread operator, make a new
 // array with numbers that contains all of the evens and odds
-
+const odds = [3, 5, 7]
+const evens = [2, 4, 6]
+const nums1 = [...odds, ...evens];
+console.log(nums1);
 
 
 //Exercise #2
@@ -14,7 +17,10 @@
 // imaginary friend what their favorite foods are. 
 // Use the spread operator to build a final allFoods with the 
 // items from the two objects you just built
-
+faveFoods = { burgers: 'Pedros', pizza: 'Davy', steak: 'House' }
+friendFaveFoods = { sandwich: 'deli', pasta: 'topped', salad: 'mixed' }
+allFoods = { ...faveFoods, ...friendFaveFoods };
+console.log(allFoods);
 
 
 // Exercise #3
@@ -30,3 +36,15 @@
 // 2) define three variables (age, breed, name) outside of the object
 // 3) add those variables to your pet
 // log the most important thing about it: the pet's name 
+myself = { name: 'Ezra', age: '41', hometown: 'Brooklyn' }
+const { name, age, ...me } = myself
+console.log(name, age, me.hometown);
+const pet = {}
+myself = { ...myself, pet }
+let petName = 'Silver'
+let petAge = 7
+let petBreed = 'Pug'
+myself.pet.Name = petName
+myself.pet.Age = petAge
+myself.pet.Breed = petBreed
+console.log(myself.pet.Name)
